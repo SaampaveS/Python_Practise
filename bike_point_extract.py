9d74860bf8e35197f0ba556f4601bbdbbb25f329
SaampaveS <166492161+SaampaveS@users.noreply.github.com>
2025-05-08 16:38:54 +0100

    Added for loop to extract all ids

--- bike_point_extract.py
+++ bike_point_extract.py
@@ -1,29 +1,39 @@
 import requests
 import json
+import os
 
-url = 'https://api.tfl.gov.uk/BikePoint/BikePoints_1'
-id = 'BikePoints_1'
-
+url = 'https://api.tfl.gov.uk/BikePoint'
 response = requests.get(url)
 
 if response.status_code== 200:
     #save the json response into a data variable
     data = response.json()
-    #print(data)
+    id = [item.get('id') for item in data]
 
-    #get modified timestamp to help us build our file. The [0] outputs tehe first modified number
-    first_value = data['additionalProperties'][0]
-    modified = first_value.get('modified')
-    modified = modified.replace(':','-')
-    modified = modified.replace('.','-')
-    #print(modified)
+    for bp in id:
+        endpoint = url + '/' + bp
+        endpoint_response = requests.get(endpoint)
+        endpoint_data = endpoint_response.json()
 
-    filename = modified+id+'.json'
-    #print(filename)
+        #get modified timestamp to help us build our file. The [0] outputs tehe first modified number
+        first_value = endpoint_data['additionalProperties'][0]
+        modified = first_value.get('modified')
+        modified = modified.replace(':','-')
+        modified = modified.replace('.','-')
+        #print(modified)
 
-    #save to a file
-    with open(filename, 'w',encoding='utf-8') as file:
-        json.dump(data,file)
+        file_list = [f for f in os.listdir('.') if f.endswith('.json')]
+        #print(file_list)
+
+        filename = modified+bp+'.json'
+        #print(filename)
+
+        if filename in file_list:
+            print('Up to date')
+        else:
+        #save to a file
+            with open(filename, 'w',encoding='utf-8') as file:
+                json.dump(endpoint_data,file)
 
 else:
     #this bit gets a cleaner error message
