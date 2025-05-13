c37e4cfacfb8504df2c556c36388f614a1b75634
SaampaveS <166492161+SaampaveS@users.noreply.github.com>
2025-05-13 16:39:03 +0100

    loaded all our files to a s3 bucket on AWS

--- all_bike_points.py
+++ all_bike_points.py
@@ -1,8 +1,24 @@
 import requests
 import json
 import os
+import boto3
+import os
+from dotenv import load_dotenv
+from load_test import load_to_s3
 
 #the initial code we had written calls the API url for each bikepoint, we want to get all 
+
+load_dotenv()
+
+access_key = os.getenv('AWS_ACCESS_KEY')
+secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+bucket = os.getenv('AWS_BUCKET_NAME')
+
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id = access_key,
+    aws_secret_access_key = secret_access_key
+)
 
 url = 'https://api.tfl.gov.uk/BikePoint'
 response = requests.get(url)
@@ -24,14 +40,18 @@ if response.status_code== 200:
         
         bp = bike_point.get('id')
 
-        file_list = [f for f in os.listdir('.') if f.endswith('.json')]
-
         filename = modified+bp+'.json'
+
+        s3_contents = s3_client.list_objects_v2(Bucket=bucket)
+        file_list = [item['Key'] for item in s3_contents.get('Contents', [])]
+
         if filename in file_list:
             print('Up to date')
         else:
             with open(filename, 'w') as file:
                 json.dump(bike_point, file)
+            load_to_s3(filename)
+            print('Uploaded '+ filename)
 else:
     #this bit gets a cleaner error message
     data = response.json()
